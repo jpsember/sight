920ce95b3209392579a26e628f63ee0f33c7035b
Jeff Sember <jpsember@gmail.com>
2024-05-12 09:50:16 -0700

    Issue #18

--- src/main/java/sight/LessonManager.java
+++ src/main/java/sight/LessonManager.java
@@ -5,11 +5,13 @@ import static sight.Util.*;
 
 import java.io.File;
 import java.util.List;
+import java.util.Random;
 
 import js.file.Files;
 import js.geometry.MyMath;
 import sight.gen.LessonCollection;
 import sight.gen.RenderedNotes;
+import sight.gen.RenderedSet;
 
 public class LessonManager {
 
@@ -47,19 +49,72 @@ public class LessonManager {
 
       var f = new File("lessons.json");
 
+      mRand = new Random(1965);
       mLessonCollection = Files.parseAbstractDataOpt(LessonCollection.DEFAULT_INSTANCE, f);
       checkArgument(mLessonCollection.renderedSets().size() != 0, "no chord sets found in lesson collection:",
           f, INDENT, mLessonCollection);
 
       for (var x : mLessonCollection.renderedSets()) {
-        var rn = chordLibrary().get(x);
-        result.add(rn);
+        var y = generateLessonsFromChordSet(x);
+        for (var z : y) {
+          var rn = chordLibrary().get(z);
+          result.add(rn);
+        }
       }
       mSets = result;
     }
     return mSets;
   }
 
+  private int[] permutation(int size) {
+    var random = mRand;
+    int[] p = new int[size];
+    for (int i = 0; i < size; i++)
+      p[i] = i;
+    for (int i = size - 1; i >= 1; i--) {
+      int j = random.nextInt(i + 1);
+      int tmp = p[i];
+      p[i] = p[j];
+      p[j] = tmp;
+    }
+    return p;
+  }
+
+  private List<RenderedSet> generateLessonsFromChordSet(RenderedSet source) {
+
+    List<RenderedSet> out = arrayList();
+
+    var noteStrs = source.notes().split(" +");
+
+    pr("split:",INDENT,source.notes(),CR,"to:",CR,noteStrs);
+    
+    final int NOTES_PER_LESSON = 4;
+    for (int perm = 0; perm < 2; perm++) {
+
+      int[] strs2 = new int[noteStrs.length];
+      for (int i = 0; i < strs2.length; i++)
+        strs2[i] = i;
+
+      if (perm != 0) {
+        strs2 = permutation(strs2.length);
+      }
+
+      for (int i = 0; i <= strs2.length - NOTES_PER_LESSON; i += NOTES_PER_LESSON) {
+        var b = source.toBuilder();
+        b.description(source.description() + " " + perm + ":" + i);
+        var sb = new StringBuilder();
+        for (var j = i; j < i + NOTES_PER_LESSON; j++) {
+          sb.append(noteStrs[strs2[j]]);
+          sb.append(' ');
+        }
+        b.notes(sb.toString().trim());
+        out.add(b.build());
+      }
+    }
+    return out;
+  }
+
+  private Random mRand;
   private List<RenderedNotes> mSets;
   private LessonCollection mLessonCollection;
   private int mLastLessonIndex = -1;
